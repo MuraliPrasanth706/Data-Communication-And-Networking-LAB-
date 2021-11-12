0e538d063e4bb19572bb48d59e4717abaf42b45e
MuraliPrasanth <83390114+MuraliPrasanth706@users.noreply.github.com>
2021-11-12 21:04:04 +0530

    Add files via upload

--- Ex-1-Simplex/client.java
+++ Ex-1-Simplex/client.java
@@ -1,1 +1,16 @@
-
+import java.io.*; 
+import java.net.*;
+public class client{
+    public static void main(String[] args) {
+        try {
+            Socket s=new Socket("localhost",5000);
+            DataInputStream di=new DataInputStream(s.getInputStream());
+            String str = (String)di.readUTF();
+            System.out.println(str);
+            s.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    
+}
---
+++ Ex-1-Simplex/server.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.net.*;
+public class server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket ss=new ServerSocket(5000);
+            Socket s=ss.accept();
+            DataOutputStream output = new DataOutputStream(s.getOutputStream());
+            String str = "I am Billa(Hangover)";
+            output.writeUTF(str);
+            output.flush();
+            output.close();
+            ss.close();
+            s.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
